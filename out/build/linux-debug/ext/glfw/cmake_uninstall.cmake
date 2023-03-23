71b3e1675921e2332455c8c76f07473b538619af
Shubham <shubhamkrs@gmail.com>
2023-03-24 01:49:35 +0530

    Created Vulkan Instance and Selected Physical Device/GPU

--- out/build/linux-debug/ext/glfw/cmake_uninstall.cmake
+++ out/build/linux-debug/ext/glfw/cmake_uninstall.cmake
@@ -1,22 +1,22 @@
 
-if (NOT EXISTS "C:/Users/prabi/source/repos/Game_02/out/build/x64-debug/ext/glfw/install_manifest.txt")
-    message(FATAL_ERROR "Cannot find install manifest: \"C:/Users/prabi/source/repos/Game_02/out/build/x64-debug/ext/glfw/install_manifest.txt\"")
+if (NOT EXISTS "C:/Users/Shubham/Desktop/Game_02/out/build/linux-debug/ext/glfw/install_manifest.txt")
+    message(FATAL_ERROR "Cannot find install manifest: \"C:/Users/Shubham/Desktop/Game_02/out/build/linux-debug/ext/glfw/install_manifest.txt\"")
 endif()
 
-file(READ "C:/Users/prabi/source/repos/Game_02/out/build/x64-debug/ext/glfw/install_manifest.txt" files)
+file(READ "C:/Users/Shubham/Desktop/Game_02/out/build/linux-debug/ext/glfw/install_manifest.txt" files)
 string(REGEX REPLACE "\n" ";" files "${files}")
 
 foreach (file ${files})
   message(STATUS "Uninstalling \"$ENV{DESTDIR}${file}\"")
   if (EXISTS "$ENV{DESTDIR}${file}")
-    exec_program("C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+    exec_program("C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
                  OUTPUT_VARIABLE rm_out
                  RETURN_VALUE rm_retval)
     if (NOT "${rm_retval}" STREQUAL 0)
       MESSAGE(FATAL_ERROR "Problem when removing \"$ENV{DESTDIR}${file}\"")
     endif()
   elseif (IS_SYMLINK "$ENV{DESTDIR}${file}")
-    EXEC_PROGRAM("C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+    EXEC_PROGRAM("C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
                  OUTPUT_VARIABLE rm_out
                  RETURN_VALUE rm_retval)
     if (NOT "${rm_retval}" STREQUAL 0)
